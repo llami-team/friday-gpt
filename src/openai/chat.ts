0a6604d661348dd5645f785c9adbb9513f0ad3ed
hmmhmmhm <hmmhmmhm@naver.com>
2023-04-11 14:11:11 +0900

    chore: Open A.I 오류시 원인 알려주도록 수정

--- src/openai/chat.ts
+++ src/openai/chat.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { isAxiosError } from "axios";
 import axiosRetry from "axios-retry";
 
 import { logger } from "../utils/logger.js";
@@ -37,21 +37,38 @@ export const chat = async (option: {
       throw new Error("ChatGPT 호출이 너무 많습니다.");
     }
   }
-  const { data } = await openai.post("/chat/completions", {
-    model: "gpt-3.5-turbo",
-    messages,
-  });
 
-  const answer = `${data.choices?.[0].message?.content ?? ""}`;
-  const finishReason = data.choices?.[0].finish_reason as string;
-
-  if (finishReason === "length") {
-    const addedChat = await chat({
-      messages: [...messages, { role: "assistant", content: answer }],
-      nested: nested ? nested + 1 : 1,
+  try {
+    const { data } = await openai.post("/chat/completions", {
+      model: "gpt-3.5-turbo",
+      messages,
     });
-    return answer + addedChat;
-  }
 
-  return answer;
+    const answer = `${data.choices?.[0].message?.content ?? ""}`;
+    const finishReason = data.choices?.[0].finish_reason as string;
+
+    if (finishReason === "length") {
+      const addedChat = await chat({
+        messages: [...messages, { role: "assistant", content: answer }],
+        nested: nested ? nested + 1 : 1,
+      });
+      return answer + addedChat;
+    }
+
+    return answer;
+  } catch (error) {
+    if (isAxiosError(error)) {
+      logger(
+        `\nOpen A.I 서버와 3회 연속 요청에 실패하였습니다. 프로그램을 종료합니다.
+다음과 같은 원인이 있을 수 있으니 확인을 부탁드립니다.
+
+1. Open A.I 인증키 유효성 문제 ( .env 파일 설정 확인 )
+2. Open A.I 서버 접속 문제 ( 인터넷 연결 확인 )
+3. Open A.I 서버 과부하 문제 ( ai.com 확인 )
+`,
+        error.response.data
+      );
+      process.exit(1);
+    }
+  }
 };
